29cb97c09361707537dabc534119d01b3689fefe
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2021-10-06 19:38:26 -0400

    Basic Structure Part 2 present. Need to figure out API call

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+function ZipCode(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        JSON.stringify(props.data)
+      }
     </div>
   );
 }
 
+function CitySearchField(props) {
+  return (
+    <div>
+      City Name:
+      <input type="text" onChange = {props.changeHandler}/>
+    </div>
+  );
+}
+
+
+class App extends Component {
+  state = {
+    cityName : '',
+    zipCodes : []
+  }
+  
+  cityChanged = (event) => {
+    //console.log(event.target.value);
+    this.setState({zipCode : event.target.value})
+    //setSate is an async call.
+    const currZip = event.target.value;
+    //if(this.validZip(currZip)){
+      fetch(`http://ctp-zip-api.herokuapp.com/city/${event.target.value}`)
+      .then((res) => res.json())
+      .then((data) => this.setState({zipCodes : data})); //this.state.zipCodes.indexOf(data) === -1 &&
+    //}
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>City Name Search</h2>
+        </div>
+        <CitySearchField zipCode = {this.state.cityName} changeHandler = {this.cityChanged}/>
+        <div>
+          Current Zip is {this.state.cityName}
+        </div>
+
+        <div>
+          {this.state.zipCodes.map((city) => <ZipCode data={city}/>)}
+        </div>
+      </div>
+    );
+  }
+}
+
 export default App;
+
