10306e76019c61f705573493f8a7420d9c288df3
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2021-10-06 22:38:18 -0400

    Logic completed. CSS needs to be done

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -29,15 +29,10 @@ class App extends Component {
   }
   
   cityChanged = (event) => {
-    //console.log(event.target.value);
-    this.setState({zipCode : event.target.value})
-    //setSate is an async call.
-    const currZip = event.target.value;
-    //if(this.validZip(currZip)){
-      fetch(`http://ctp-zip-api.herokuapp.com/city/${event.target.value}`)
-      .then((res) => res.json())
-      .then((data) => this.setState({zipCodes : data})); //this.state.zipCodes.indexOf(data) === -1 &&
-    //}
+    this.setState({zipCode : event.target.value});
+    fetch(`http://ctp-zip-api.herokuapp.com/city/${event.target.value.toUpperCase()}`)
+    .then((res) => res.json())
+    .then((data) => this.setState({zipCodes : data}));
   }
   
   render() {
@@ -47,10 +42,7 @@ class App extends Component {
           <h2>City Name Search</h2>
         </div>
         <CitySearchField zipCode = {this.state.cityName} changeHandler = {this.cityChanged}/>
-        <div>
-          Current Zip is {this.state.cityName}
-        </div>
-
+        
         <div>
           {this.state.zipCodes.map((city) => <ZipCode data={city}/>)}
         </div>
